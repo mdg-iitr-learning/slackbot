20a686e99c30aa61efb45225bf7068b7ea1677c8
yash <yp969803@gmail.com>
2023-05-07 02:10:11 +0530

    Slackbot ready

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
-const Slackbot=require('slackbots')
+const SlackBot=require('slackbots')
 const axios=require('axios')
 const dotenv=require('dotenv')
 dotenv.config()
+const bot = new SlackBot({
+    token: `${process.env.BOT_TOKEN}`,
+    name: 'deca'
+})
+
+bot.on('start', () => {
+    const params = {
+        icon_emoji: ':robot_face:'
+    }
+
+    bot.postMessageToChannel(
+        'random',
+        'Get inspired while working with @deca',
+        params
+    );
+    
+})
+bot.on('message', (data) => {
+  
+    const params = {
+        icon_emoji: ':male-technologist:'
+    }
+    if(data.type== 'desktop_notification'&&(data.title)=='deca') {
+        
+        bot.postMessageToChannel(
+                data.subtitle.slice(1,),
+                data.content.replace('@deca',''),
+                params
+            )
+    }
+   
+})
+bot.on('error',(err)=>{
+    console.log(err)
+})
+
+
